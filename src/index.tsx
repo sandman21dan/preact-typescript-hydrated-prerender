cfd9498369613f97f0fcc4064599e215c6b116de
Daniel Perez <danielperezvl@gmail.com>
2019-03-13 19:02:57 +0000

    Fix pre-rendering app bootstraping

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { h, render } from 'preact';
 import App from './App';
 
-const app = document.getElementById('app');
+const root = document.getElementById('root');
+const app = document.querySelector('.app');
 
-render(<App />, document.body, app);
+render(<App />, root, app);
