37159f8e66a684b913d59e85f381f60849971538
Daniel Perez <danielperezvl@gmail.com>
2019-12-15 13:04:19 +0000

    feat: add routing links

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -1,9 +1,22 @@
 import {h, FunctionalComponent } from 'preact'
+import { Link } from 'preact-router/match';
 import './Header.scss';
 
 export const Header: FunctionalComponent = () => (
-  <header class="app-header">
-    <a class="app-header__link" href="/">Home</a>
-    <a class="app-header__link" href="/about">About</a>
-  </header>
+  <nav class="app-nav">
+    <Link
+      class="app-nav__link"
+      activeClassName="app-nav__link--active"
+      href="/"
+    >
+      Home
+    </Link>
+    <Link
+      class="app-nav__link"
+      activeClassName="app-nav__link--active"
+      href="/about"
+    >
+      About
+    </Link>
+  </nav>
 );
